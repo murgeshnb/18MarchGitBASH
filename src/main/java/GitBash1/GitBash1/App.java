4d0e60c370cb4bd6ca5530cef5d0a7187d303fd1
murgeshnb <mnbechetti@gmail.com>
2022-03-18 22:14:07 +0530

    committing message for first time

---
+++ src/main/java/GitBash1/GitBash1/App.java
@@ -0,0 +1,13 @@
+package GitBash1.GitBash1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/package1/PgmA.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class PgmA {
+
+	public static void main(String[] args) {
+		System.out.println("Gitbash practice 1");
+
+	}
+
+}
